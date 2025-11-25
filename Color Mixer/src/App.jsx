c22710aee5e22fa58a5ff8df946101706ba2135a
Adeelniaz7 <ADEEL7Niaz@gmail.com>
2025-11-25 23:18:23 +0500

    first commit

---
+++ Color Mixer/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function App() {
+  const colors=JSON.parse(localStorage.getItem('color'))
+  const [r,setR]=useState(colors && colors.r?colors.r:0);
+  const [g,setG]=useState(colors && colors.g?colors.g:0);
+  const [b,setB]=useState(colors && colors.b?colors.b:0);
+
+  const save=()=>{
+    localStorage.setItem("color",JSON.stringify({r,g,b}))
+  }
+
+  return (
+    
+    <div>
+      <h1>Color Mixer</h1>
+      <div style={{backgroundColor:'rgb('+r+','+g+','+b+')', height:200, width:200}}></div>
+      <label htmlFor="">Red</label>
+      <input  value={r} type="range" onChange={(event)=>setR(event.target.value)} min={0} max={255}></input>
+      <br/>
+      <br/>
+      <label htmlFor="">Green</label>
+      <input value={g} type="range" onChange={(event)=>setG(event.target.value)} min={0} max={255}></input>
+      <br/>
+      <br/>
+      <label htmlFor="">Blue</label>
+      <input value={b} type="range" onChange={(event)=>setB(event.target.value)} min={0} max={255}></input>
+      <button onClick={save}>Save Color Combination</button>
+    </div>
+  )
+}
+
+
+export default App;
